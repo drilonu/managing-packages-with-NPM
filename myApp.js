8513a388048a8052e129caa1fc6fa7180c7663d7
drilonu <86578785+drilonu@users.noreply.github.com>
2022-01-17 02:37:50 +0000

    Changed to arrow functions

--- myApp.js
+++ myApp.js
@@ -19,7 +19,7 @@ bGround.log("Hello World");
 
 
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
 
@@ -29,7 +29,7 @@ app.get("/", function(req, res) {
 app.use('/public', express.static(__dirname + "/public"));
 
 
-app.get("/json", function(req, res) {
+app.get("/json", (req, res) => {
     res.json(
         { "message": "Hello json" }
 );
