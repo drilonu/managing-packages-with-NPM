eb071a240a8ae463a1dbce2e28de068327869938
drilonu <86578785+drilonu@users.noreply.github.com>
2022-01-17 02:32:59 +0000

    Add a json route with dummy data

--- myApp.js
+++ myApp.js
@@ -29,7 +29,11 @@ app.get("/", function(req, res) {
 app.use('/public', express.static(__dirname + "/public"));
 
 
-
+app.get("/json", function(req, res) {
+    res.json(
+        { "message": "Hello json" }
+);
+});
 
 
 
