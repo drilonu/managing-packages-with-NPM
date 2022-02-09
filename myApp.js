4a7303a433e70b1a5da022c611ee8935324d4b3f
drilonu <86578785+drilonu@users.noreply.github.com>
2022-02-09 18:20:03 +0000

    Add dotenv package and connect it to myApp

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
+require('dotenv').config()
 
 
 
@@ -18,21 +19,32 @@ bGround.log("Hello World");
 
 
 
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-  });
-
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/views/index.html");
+//   });
 
 
-  app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+// app.use('/public', express.static(__dirname + "/public"));
 
 
-app.get("/json", (req, res) => {
+// app.get("/json", (req, res) => {
+//     res.json(
+//         { "message": "Hello json" }
+// );
+// });
+
+app.get("/json", function(req, res) {
+  console.log(process.env.MESSAGE_STYLE, " <= message style");
+  if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json(
-        { "message": "Hello json" }
-);
+      { "mesage": "HELLO JSON"}
+    )
+  } else {
+    res.json(
+      { "mesage": "Hello json"}
+    )
+  }
 });
 
 
