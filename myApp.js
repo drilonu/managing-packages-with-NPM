f911e935dcd3d5dbea184f4092632685a8e96008
drilonu <86578785+drilonu@users.noreply.github.com>
2022-02-11 14:34:18 +0000

    Add an echo API path 2

--- myApp.js
+++ myApp.js
@@ -59,7 +59,7 @@ app.get("/now", function(req, res, next) {
 })
 
 
-app.get("/:word/echo", function(req, res) {
+app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
