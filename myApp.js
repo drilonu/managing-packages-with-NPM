e570a11bdc1b279b3e3d8403ceeadf9d67f1ff5c
drilonu <86578785+drilonu@users.noreply.github.com>
2022-02-11 13:53:51 +0000

    Add a middleware logger

--- myApp.js
+++ myApp.js
@@ -4,7 +4,10 @@ var app = express();
 var bGround = require('fcc-express-bground');
 require('dotenv').config()
 
-
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " " + req.ip)
+  next();
+});
 
 
 
@@ -20,20 +23,20 @@ bGround.log("Hello World");
 
 
 
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/views/index.html");
-//   });
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
-// app.use(express.static(__dirname + "/public"));
-// app.use('/public', express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + "/public"));
 
 
-// app.get("/json", (req, res) => {
-//     res.json(
-//         { "message": "Hello json" }
-// );
-// });
+app.get("/json", (req, res) => {
+    res.json(
+        { "message": "Hello json" }
+);
+});
 
 app.get("/json", function(req, res) {
   var jsonResponse = { "message": "Hello json"};
