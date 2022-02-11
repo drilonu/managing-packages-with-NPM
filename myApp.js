e3998bc8c69f62b9c6c034ee349f4b3d97e84837
drilonu <86578785+drilonu@users.noreply.github.com>
2022-02-11 14:18:58 +0000

    Add /now API response

--- myApp.js
+++ myApp.js
@@ -46,6 +46,18 @@ app.get("/json", function(req, res) {
    res.json(jsonResponse);
 });
 
+function getTheCurrentTimeString() {
+  return new Date().toString();
+}
+
+app.get("/now", function(req, res, next) {
+  
+  req.time = getTheCurrentTimeString();
+  next(); 
+}, function(req, res) {
+  res.json({ time: req.time });
+})
+
 
 
 
